b9bfe90d6350ea8182bf732e33206451cb4569b9
Daniel <81522135+Glaciian@users.noreply.github.com>
2024-08-17 14:49:15 +1000

    Macos test update

--- Hedron/src/Hedron/Core.h
+++ Hedron/src/Hedron/Core.h
@@ -2,19 +2,20 @@
 
 // Core Build Architecture.
 
-// Definitions for Windows platforms.
-#ifdef HDE_PLATFORM_WINDOWS
-	#ifdef HDE_BUILD_DLL
-		// If HDE_BUILD_DLL is defined as HDE_PLATFORM_WINDOWS, define HEDRON_API as __declspec(dllimport).
-		// Symbols following this macro are being exported from the DLL.
-		#define HEDRON_API __declspec(dllexport) 
-	#else
-		// If HDE_BUILD_DLL is not defined, define HEDRON_API as __declspec(dllimport).
-		// Symbols following this macro are being imported from the DLL.
-		#define HEDRON_API __declspec(dllimport)
-	#endif
+#if defined(HDE_PLATFORM_WINDOWS)
+#if defined(HDE_BUILD_DLL)
+    // Symbols following this macro are being exported from the DLL.
+    #define HEDRON_API __declspec(dllexport)
 #else
-	// Compile-time error if Hedron doesn't support the current system architecture.
-	#error Hedron doesn't support the current system architecture.
+    // Symbols following this macro are being imported from the DLL.
+    #define HEDRON_API __declspec(dllimport)
 #endif
-
+#elif defined(HDE_PLATFORM_MACOS)
+#if defined(HDE_BUILD_DLL)
+    // Symbols following this macro are being exported from the DLL.
+    #define HEDRON_API __attribute__((visibility("default")))
+#endif
+#else
+    // Compile-time error if Hedron doesn't support the current system architecture.
+    #error "Hedron doesn't support the current system architecture."
+#endif
--- Hedron/src/Hedron/EntryPoint.h
+++ Hedron/src/Hedron/EntryPoint.h
@@ -23,4 +23,24 @@ int main(int argc, char** argv)
 	delete app;	
 }
 
-#endif	0
+#elif defined(HDE_PLATFORM_MACOS)
+	
+// Function prototype defined by sandBox for creating an application instance.
+extern Hedron::Application* Hedron::CreateApplication();
+
+// Entry Point for Applications
+int main(int argc, char** argv)
+{
+	// Initialization of the logger.
+	// Should be moved to an initialization function.
+	Hedron::Log::Init();
+	HDE_CORE_INFO("Core Logger Initialized.");
+	HDE_APP_INFO("Application Logger Initialized.");
+
+	// Application creation, execution and cleanup.
+	auto app = Hedron::CreateApplication();
+	app->Run();
+	delete app;
+}
+
+#endif
