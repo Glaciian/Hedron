6a022b27d0d4e41aa9cd5979e837d090fe13135a
Scimitar5 <jlcamino@me.com>
2024-08-18 18:31:48 +1000

    Xcode test

--- Hedron/src/Hedron/EntryPoint.h
+++ Hedron/src/Hedron/EntryPoint.h
@@ -39,9 +39,7 @@ int main(int argc, char** argv)
 	HDE_CORE_INFO("Core Logger Initialized just because.");
 	HDE_APP_INFO("Application Logger Initialized.");
 	HDE_APP_INFO("Ain't this all cool!!");
-	HDE_APP_INFO("APP INFO Example");
-	HDE_APP_WARN("APP WARN Example");
-	HDE_APP_DEBUG("APP DEBUG Example");
+	HDE_APP_INFO("App Update Test");
 
 	// Application creation, execution and cleanup.
 	auto app = Hedron::CreateApplication();
